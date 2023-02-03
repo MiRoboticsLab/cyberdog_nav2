0185e3ae6b0d2cd99f0998fbaad2170247e151a2
duyongquan <duyongquan1@xiaomi.com>
2023-02-03 12:24:13 +0800

    [Fix] fix ci
    	1 fix CI
    
    Signed-off-by: duyongquan <duyongquan1@xiaomi.com>

--- map_label_server/src/labelserver_node.cpp
+++ map_label_server/src/labelserver_node.cpp
@@ -179,11 +179,11 @@ void LabelServer::HandleSetLabelServiceCallback(
       INFO("Removing label: %s", label.label_name.c_str());
       bool success = label_store_->RemoveLabel(label_filename, label.label_name.c_str());
       if (!success) {
-        ERROR("Remove label: %s failed",  label.label_name.c_str());
+        ERROR("Remove label: %s failed", label.label_name.c_str());
         response->success = protocol::srv::SetMapLabel_Response::RESULT_FAILED;
         return;
       } else {
-        INFO("Remove label: %s success",  label.label_name.c_str());
+        INFO("Remove label: %s success", label.label_name.c_str());
       }
     }
     response->success = protocol::srv::SetMapLabel_Response::RESULT_SUCCESS;
