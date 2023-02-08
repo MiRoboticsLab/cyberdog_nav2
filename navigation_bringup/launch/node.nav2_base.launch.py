8eb4344b05432547af7733521ebe0172dcdfe817
田洪海 <tianhonghai@126.com>
2023-02-08 21:35:43 +0800

    [Modify]: reconfig launch file of nav2 base to test
    Signed-off-by: 田洪海 <tianhonghai@126.com>

--- navigation_bringup/launch/node.nav2_base.launch.py
+++ navigation_bringup/launch/node.nav2_base.launch.py
@@ -34,6 +34,7 @@ from nav2_common.launch import RewrittenYaml
 
 def generate_launch_description():
 
+######################## tracking ##############################
     namespace = LaunchConfiguration('namespace', default='')
     namespace_declare = DeclareLaunchArgument(
         name='namespace',
@@ -49,7 +50,6 @@ def generate_launch_description():
             os.path.join(package_dir, 'params'),
             'follow_params.yaml')
     )
-
     default_target_tracking_bt_xml = LaunchConfiguration(
         'default_target_tracking_bt_xml',
         default=os.path.join(
@@ -64,15 +64,13 @@ def generate_launch_description():
         'default_target_tracking_bt_xml': default_target_tracking_bt_xml,
         'map_subscribe_transient_local': map_subscribe_transient_local
         }
-
     configured_params_f = RewrittenYaml(
             source_file=follow_params_file,
             root_key=namespace,
             param_rewrites=param_substitutions,
             convert_types=True
             )
-    
-    controller_cmd = Node(
+    controller_tracking_cmd = Node(
             package='mcr_controller',
             executable='controller_server',
             name='controller_server_tracking',
@@ -81,7 +79,7 @@ def generate_launch_description():
             parameters=[{configured_params_f}],
             remappings=remappings
             )
-    planner_cmd = Node(
+    planner_tracking_cmd = Node(
             package='mcr_planner',
             executable='mcr_planner_server',
             name='planner_server_tracking',
@@ -90,7 +88,7 @@ def generate_launch_description():
             parameters=[{configured_params_f}],
             remappings=remappings
             )
-    recoveries_cmd = Node(
+    recoveries_tracking_cmd = Node(
             package='nav2_recoveries',
             executable='recoveries_server',
             name='recoveries_server',
@@ -99,7 +97,7 @@ def generate_launch_description():
             parameters=[{configured_params_f}],
             remappings=remappings
             )
-    bt_navigator_cmd = Node(
+    bt_navigator_tracking_cmd = Node(
             package='bt_navigators',
             executable='bt_navigator_tracking',
             name='bt_navigator_tracking',
@@ -108,20 +106,145 @@ def generate_launch_description():
             parameters=[{configured_params_f}],
             remappings=remappings
             )
-    # map_server_cmd = Node(
-    #         package='nav2_map_server',
-    #         executable='map_server',
-    #         name='map_server',
-    #         namespace=namespace,
-    #         output='screen',
-    #         parameters=[{'yaml_filename': map_file}],
-    #         remappings=remappings)
+
+######################## tracking ##############################
+
+######################## docking ##############################
+
+    auto_docking_file = LaunchConfiguration(
+        'auto_charing_file',
+        default=os.path.join(
+            os.path.join(package_dir, 'params'),
+            'auto_docking.yaml')
+        )
+    default_auto_docking_bt_xml = LaunchConfiguration(
+        'default_auto_docking_bt_xml',
+        default=os.path.join(
+            os.path.join(package_dir, 'behavior_trees'),
+            'auto_docking.xml')
+    )
+    map_subscribe_transient_local = LaunchConfiguration(
+        'map_subscribe_transient_local',
+        default='true'
+    )
+    param_substitutions = {
+        # 'default_target_tracking_bt_xml': default_target_tracking_bt_xml,
+        'default_auto_docking_bt_xml': default_auto_docking_bt_xml,
+        'map_subscribe_transient_local': map_subscribe_transient_local
+        }
+    configured_params_a = RewrittenYaml(
+            source_file=auto_docking_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True)
+    controller_docking_cmd = Node(
+            package='mcr_controller',
+            executable='controller_server',
+            name='controller_server_docking',
+            namespace=namespace,
+            output='screen',
+            parameters=[{configured_params_a}],
+            remappings=remappings
+            )
+    planner_docking_cmd = Node(
+            package='mcr_planner',
+            executable='mcr_planner_server',
+            name='planner_server_docking',
+            namespace=namespace,
+            output='screen',
+            parameters=[{configured_params_a}],
+            remappings=remappings
+            )
+    recoveries_docking_cmd = Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            namespace=namespace,
+            output='screen',
+            parameters=[{configured_params_a}],
+            remappings=remappings
+            )
+    bt_navigator_docking_cmd = Node(
+            package='bt_navigators',
+            executable='bt_navigator_docking',
+            name='bt_navigator_docking',
+            namespace=namespace,
+            output='screen',
+            parameters=[{configured_params_a}],
+            remappings=remappings
+            )
+
+######################## docking ##############################
+
+######################## ab ##############################
+
+    namespace = LaunchConfiguration('namespace', default='')
+    params_file = LaunchConfiguration('params_file',
+            default=os.path.join(package_dir, 'params', 
+            'navigate_to_pose_params.yaml'))
+
+    configured_params = RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            # param_rewrites=param_substitutions,
+            convert_types=True)
+
+
+    # SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+
+    bt_navigator_ab_cmd = Node(
+            package='bt_navigators',
+            executable='bt_navigator_pose',
+            name='bt_navigator_ab',
+            output='screen',
+            parameters=[configured_params],
+            namespace=namespace,
+            remappings=remappings),
+
+    controller_ab_cmd  =  Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server_ab',
+            output='screen',
+            parameters=[configured_params],
+            namespace=namespace,
+            remappings=remappings),
+
+    planner_ab_cmd =    Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server_ab',
+            output='screen',
+            parameters=[configured_params],
+            namespace=namespace,
+            remappings=remappings),
+
+    recoveries_ab_cmd  =  Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            output='screen',
+            parameters=[configured_params],
+            namespace=namespace,
+            remappings=remappings
+        ),
+
+######################## ab ##############################
+
     ld = launch.LaunchDescription([
         namespace_declare,
-        controller_cmd,
-        planner_cmd,
-        recoveries_cmd,
-        bt_navigator_cmd,
+        controller_tracking_cmd,
+        planner_tracking_cmd,
+        recoveries_tracking_cmd,
+        bt_navigator_tracking_cmd,
+        controller_docking_cmd,
+        planner_docking_cmd,
+        recoveries_docking_cmd,
+        bt_navigator_docking_cmd,
+        controller_ab_cmd,
+        planner_ab_cmd,
+        recoveries_ab_cmd,
+        bt_navigator_ab_cmd,
         # map_server_cmd
     ])
     return ld
